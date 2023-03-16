4a52771977cf5552a801afa853a48b13dae9ce4d
gorjola <ggipian@gmail.com>
2023-03-17 02:14:57 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        for(int i=1;i<=10;i++){
+            System.out.println();
+            for(int j=10;j>=i;j--){
+                System.out.print("x");
+            }
+        }
+
+
+
+
+    }
+}
